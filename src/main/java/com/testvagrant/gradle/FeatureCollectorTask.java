2a9c05c8335d775e0fdde6bb9d0fadd69bad863c
krishnb <kbandame@gmail.com>
2017-02-04 11:38:10 +0530

    Stable Build

--- src/main/java/com/testvagrant/gradle/FeatureCollectorTask.java
+++ src/main/java/com/testvagrant/gradle/FeatureCollectorTask.java
@@ -3,6 +3,7 @@ package com.testvagrant.gradle;
 
 import com.testvagrant.core.FeatureFilter;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.OutputFiles;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.File;
@@ -10,20 +11,30 @@ import java.util.*;
 
 public class FeatureCollectorTask extends DefaultTask{
 
+    private Collection<File> featureFiles = new ArrayList<>();
+
     @TaskAction
-    public Collection<File> collectFeatures() {
+    public void collectFeatures() {
         FeatureCollectorExtension featureCollectorExtension = getProject().getExtensions().findByType(FeatureCollectorExtension.class);
         if(featureCollectorExtension ==null) {
             featureCollectorExtension = new FeatureCollectorExtension();
         }
+        System.out.println("Feature Tags -- "+featureCollectorExtension.getTags());
         List<String> tags = getTags(featureCollectorExtension.getTags());
         FeatureFilter featureFilter = new FeatureFilter(tags);
-        List<File> featureFiles = featureFilter.collectAllFeatureFilesInProject(getProject().getProjectDir().listFiles());
-        List<File> files = featureFilter.getFilteredFeatures(featureFiles);
-        files.forEach(file -> System.out.println(file.getName()));
-        return files;
+        List<File> featureFilesList = featureFilter.collectAllFeatureFilesInProject(getProject().getProjectDir().listFiles());
+        featureFiles = featureFilter.getFilteredFeatures(featureFilesList);
+        featureFiles.forEach(file -> System.out.println(file.getName()));
     }
 
+    @OutputFiles
+    public Collection<File> getFeatureFiles() {
+        return featureFiles;
+    }
+
+    public void setFeatureFiles(Collection<File> featureFiles) {
+        this.featureFiles = featureFiles;
+    }
 
     private List<String> getTags(String tags) {
         if(!tags.contains(",")) {
